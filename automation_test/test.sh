1c9885a5b17101ec005f70b6868ea902c2a0e8fd
BrandonHuynhProfessional <brand.danh.huynh@gmail.com>
2024-02-28 03:22:18 -0800

    Updated automation scripts to properly test on SPN_Reference_DB files

--- automation_test/test.sh
+++ automation_test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-remote_repo="https://github.com/BrandonHuynhProfessional/strepAutomationTest/" 
+reference_DB_path="../SPN_Reference_DB"
 
 # Read the list of required files from config.json
 required_files=($(jq -r '.start[]' config.json))
@@ -8,10 +8,10 @@ required_files=($(jq -r '.start[]' config.json))
 # Error Handling for list of files
 missing_files=false  # Flag to track if any files are missing
 
-for file in ${required_files[@]}; do 
-    if [ ! -f "$file" ]; then
+for file in ${required_files[@]}; do
+    if [ ! -f "$reference_DB_path/$file" ]; then
         echo "Error: File $file specified in config.json does not exist."
-        missing_files=true  
+        missing_files=true
     fi
 done
 
@@ -24,17 +24,17 @@ fi
 git fetch origin
 
 for file in ${required_files[@]}; do
-    git checkout origin/main -- "$file"
+    git checkout origin/spn_ref_autoUpdate -- "$reference_DB_path/$file"
     echo "Checking out $file"
 done
 
 # Output file update dates to run data
-run_data_file="run_data.json"  
-echo '{"run_data": [' > "$run_data_file" 
+run_data_file="run_data.json"
+echo '{"run_data": [' > "$run_data_file"
 
 first_file=true  # Flag to track if we're adding the first entry
 for file in ${required_files[@]}; do
-    last_commit_date=$(git log -1 --format="%cd" --date=iso8601 -- "$file")
+    last_commit_date=$(git log origin/spn_ref_autoUpdate -1 --format="%cd" --date=iso8601 -- "$reference_DB_path/$file")
 
     # Add comma before entries except the first
     if [ "$first_file" = false ]; then 
@@ -46,5 +46,3 @@ for file in ${required_files[@]}; do
 done
 
 echo ']}' >> "$run_data_file"  
-
-# Eventually add in error handling in case we are missing files from config
